e1ed4c16f88db868241edca4a8899c9eb82232c7
Astupidbirdwhowantstolearn <2753879382@qq.com>
2023-05-01 10:18:29 +0800

    第五次提交

--- src/main/java/git/hello.java
+++ src/main/java/git/hello.java
@@ -3,9 +3,7 @@ package git;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello no.4");
-				
+		System.out.println("hello no.5");				
 	}
 
 }
